e808dfc24ce77c04c32604c17b568d1df2434028
MarioBrandao0 <mariorochedo329@gmail.com>
2026-07-06 13:48:19 -0300

    renomeando o controller de tranferencia para transação

--- src/main/java/com/banco/basico/simulador/controllers/ControllerTransacao.java
+++ src/main/java/com/banco/basico/simulador/controllers/ControllerTransacao.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/transferencia")
-public class ControllerTransferencia {
+@RequestMapping("/transacao")
+public class ControllerTransacao {
     //Desenvolver isso aqui
 }
